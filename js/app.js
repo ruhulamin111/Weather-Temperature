8083ef15302c20283b2f08750c25ad4fbcd73826
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-13 16:14:22 +0600

    output result done

--- js/app.js
+++ js/app.js
@@ -4,30 +4,24 @@ const searchCity = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayOutput(data))
+}
+const setValue = (id, value) => {
+    document.getElementById(id).innerText = value;
+}
+const displayOutput = data => {
+    console.log(data.weather[0].main)
+    setValue('city', data.name);
+    setValue('temperature', data.main.temp);
+    setValue('condition', data.weather[0].main);
 }
 
 
-/* const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
-const searchTemperature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayTemperature(data));
-}
+// const displayTemperature = temperature => {
 
-const setInnerText = (id, text) => {
-    document.getElementById(id).innerText = text;
-}
-
-const displayTemperature = temperature => {
-    setInnerText('city', temperature.name);
-    setInnerText('temperature', temperature.main.temp);
-    setInnerText('condition', temperature.weather[0].main);
-    // set weather icon
-    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-    const imgIcon = document.getElementById('weather-icon');
-    imgIcon.setAttribute('src', url);
-} */
+//     // set weather icon
+//     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+//     const imgIcon = document.getElementById('weather-icon');
+//     imgIcon.setAttribute('src', url);
+// }
