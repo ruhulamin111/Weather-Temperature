68fee1be6646aa96ffd3ee8020c9a83a4ba971e2
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-13 18:31:27 +0600

    solve the problem weather

--- js/app.js
+++ js/app.js
@@ -5,23 +5,20 @@ const searchCity = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayOutput(data))
+
 }
 const setValue = (id, value) => {
     document.getElementById(id).innerText = value;
 }
 const displayOutput = data => {
-    console.log(data.weather[0].main)
+
     setValue('city', data.name);
     setValue('temperature', data.main.temp);
+    console.log(data.main.temp)
     setValue('condition', data.weather[0].main);
-}
 
+    const url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const icon = document.getElementById('weather-icon');
+    icon.setAttribute('src', url);
 
-
-// const displayTemperature = temperature => {
-
-//     // set weather icon
-//     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-//     const imgIcon = document.getElementById('weather-icon');
-//     imgIcon.setAttribute('src', url);
-// }
+};
